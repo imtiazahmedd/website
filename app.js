4c1688b5c55ed1a1a0d05cacb3464f35d4a7ad77
imtiazahmedd <imtiazpak65@gmail.com>
2017-01-03 20:08:47 -0800

    form validation;

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-//  Registration page coding
+// ******************** Registration page coding ***************************
 
 var arr = [];
+
 
 function register(){
 var firstName =document.getElementById('firstNameId').value;
@@ -8,6 +9,41 @@ var lastName = document.getElementById('lastNameId').value;
 var userName = document.getElementById('userNameId').value;
 var emailAddress = document.getElementById('emailId').value;
 var password = document.getElementById('passwordId').value;
+var password_confirm = document.getElementById('password_confirmation').value;
+
+
+// **********************    form validation    *****************************
+if(firstName == ''){
+document.getElementById('formvalidation').innerHTML = 'Please Enter Your Name';
+
+return;
+}
+else if(lastName == ''){
+document.getElementById('formvalidation').innerHTML = 'Please Enter Your Last Name';
+return;
+}
+else if(userName == ''){
+document.getElementById('formvalidation').innerHTML = 'Please Enter Your User Name';
+return;
+} 
+else if(emailAddress == ''){
+document.getElementById('formvalidation').innerHTML = 'Please Enter Your Email like exapmle@gmail.com';
+return;
+}
+else if(password == ''){
+document.getElementById('formvalidation').innerHTML = 'Please Enter Your Password';
+return;
+}
+else{
+    if(password_confirm == ''){
+document.getElementById('formvalidation').innerHTML = 'Please Enter Your confirmation password';
+
+return;
+    }
+    
+}
+
+
 
 var objFiled = { 
     userFirstName : firstName , 
@@ -17,9 +53,11 @@ var objFiled = {
     userPassword : password
 };
 arr.push(objFiled);
-console.log(arr);
+console.log(arr); 
 localStorage.setItem( "Registration" , JSON.stringify(arr));
 sweetAlert("Done...", "Registration completed" ,"success");
+
+// *****************empty input field *******************************
 
 document.getElementById('firstNameId').value = '';
 document.getElementById('lastNameId').value = '';
@@ -28,4 +66,10 @@ document.getElementById('emailId').value = '';
 document.getElementById('passwordId').value = '';
 document.getElementById('password_confirmation').value = '';
 
+}
+function cleartxt(){
+document.getElementById('formvalidation').innerHTML = '';
+    
+
+   
 }
